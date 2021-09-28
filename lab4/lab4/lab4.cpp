df671b150aeb52ef19ffb7db08a093a13e459a2a
Alena <starodubova.alena.68@gmail.com>
2021-09-29 02:35:27 +0300

    lab4/2

---
+++ lab4/lab4/lab4.cpp
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+int main()
+{
+	int  d;
+	setlocale(LC_ALL, "Rus");
+	printf("Диаметр окружности:");
+	scanf("%d", &d);
+	if (d >= 0)
+	{
+		;
+		printf("Длина окружности: %.3f\n", (float)d * 3.14);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+
+}
