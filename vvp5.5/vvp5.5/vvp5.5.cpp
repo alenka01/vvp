78d2a0e17adf7b9a58ce49554ad0633af8d3b417
Alena <starodubova.alena.68@gmail.com>
2021-09-29 09:19:01 +0300

    vvp5/5

---
+++ vvp5.5/vvp5.5/vvp5.5.cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+int main()
+{
+	int x1, x2, y1, y2, x3, y3;
+	float k, l, m, p;
+	setlocale(LC_ALL, "Rus");
+	printf("Введите координаты одной точки");
+	scanf("%d%d", &x1, &y1);
+	printf("Введите координаты второй точки");
+	scanf("%d%d", &x2, &y2);
+	printf("Введите координаты второй точки");
+	scanf("%d%d", &x3, &y3);
+	k = (float)sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+	m = (float)sqrt((x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2));
+	l = (float)sqrt((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1));
+	p = (float)(k + m + l) / 2;
+	printf("Площадь треугольника: %f", sqrt(p * (p - l) * (p - k) * (p - m)));
+	printf("Периметр треугольника: %f", k+m+l);
+}
+
