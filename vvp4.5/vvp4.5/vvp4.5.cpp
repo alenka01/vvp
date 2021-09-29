8f8105964e63ee30320e5c7bef64fd42c61b62f1
Alena <starodubova.alena.68@gmail.com>
2021-09-29 08:16:49 +0300

    vvp4/5

---
+++ vvp4.5/vvp4.5/vvp4.5.cpp
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+int main()
+{
+	int a, b;
+	setlocale(LC_ALL, "Rus");
+	printf("Введите числа");
+	scanf("%d %d", &a, &b);
+	printf("Сумма модулей %d\n", abs(a) + abs(b));
+	printf("Произведение модулей %d\n", abs(a) * abs(b));
+	printf("Частное модулей %f\n", (float)abs(a) / abs(b));
+	printf("Разность модулей %d\n", abs(a) - abs(b));
+}
