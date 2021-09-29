a8be33daeb2a1a3d14320fb102e28d0cd7f7f05f
Alena <starodubova.alena.68@gmail.com>
2021-09-29 09:19:48 +0300

    vvp5/1

---
+++ vvp5.1/vvp5.1/vvp5.1.cpp
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+int main()
+{
+	int x1, x2, y1, y2;
+	setlocale(LC_ALL, "Rus");
+	printf("Введите координаты одной точки");
+	scanf("%d%d", &x1, &y1);
+	printf("Введите координаты второй точки");
+	scanf("%d%d", &x2, &y2);
+	printf("Расстояние между точками: %.3f\n", sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
+
+}
