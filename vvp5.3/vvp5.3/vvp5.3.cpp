00c1d585a14703cb05e5a2632f68a32c9e75ab28
Alena <starodubova.alena.68@gmail.com>
2021-09-29 08:25:41 +0300

    vvp5/3

---
+++ vvp5.3/vvp5.3/vvp5.3.cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+int main()
+{
+	int a, b, c;
+	setlocale(LC_ALL, "Rus");
+	printf("Координаты первой точки(a)");
+	scanf("%d", &a);
+	printf("Координаты второй точки(b)");
+	scanf("%d", &b);
+	printf("Координаты третьей точки(c)");
+	scanf("%d", &c);
+	if (c>a && c<b)
+	{
+		printf("Произведение длин отрезков: %d", abs(a - c) * abs(b - c));
+	}
+	else
+	{
+		printf("Не подходит по условию\n");
+	}
+	
+}
