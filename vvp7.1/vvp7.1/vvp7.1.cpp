3e5cc934b6fdd6246ce72cf61f0fd05046803eb2
Alena <starodubova.alena.68@gmail.com>
2021-10-02 17:14:58 +0300

    vvp7.1

---
+++ vvp7.1/vvp7.1/vvp7.1.cpp
@@ -0,0 +1,13 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+#include <locale.h>
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	float a,m;
+	printf("Введите угол");
+	scanf("%f", &a);
+	m =(float) a*3.14/180;
+	printf("Значение в радианах: %f\n", m);
+
+}
