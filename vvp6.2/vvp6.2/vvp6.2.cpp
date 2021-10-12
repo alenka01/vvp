a4b7aed24751e2df7e74d1d680bc11e4437c2fd1
Alena <starodubova.alena.68@gmail.com>
2021-10-12 18:08:46 +0300

    vvp6.2

---
+++ vvp6.2/vvp6.2/vvp6.2.cpp
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+#include <locale.h>
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int a, b, c, k;
+    printf("Введите три числа:");
+    scanf("%d %d %d", &a, &b, &c);
+    k = a;
+    a = b;
+    b = c;
+    c = k;
+    printf("Ответ %d %d %d\n", a, b, c);
+}
