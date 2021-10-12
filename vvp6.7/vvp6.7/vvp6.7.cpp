a927a21fb0c0cd9cf53ad54a55f272cca2d71359
Alena <starodubova.alena.68@gmail.com>
2021-10-12 18:40:27 +0300

    vvp6.7

---
+++ vvp6.7/vvp6.7/vvp6.7.cpp
@@ -0,0 +1,14 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+#include <locale.h>
+#include <cmath>
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    float a, y, x;
+    printf("Введите значение A: ");
+    scanf("%f", &a);
+    y = a * a * a * a * a * a;
+    x = a * a * a * a * a * a;
+    printf("Ответ %f\n", y * x * a * a * a);
+}
