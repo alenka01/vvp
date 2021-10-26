1da8e6f9725f4500b561b8a7ccf1fe1ba5138945
starodubova <90762340+alenka01@users.noreply.github.com>
2021-10-26 23:42:34 +0300

    Add files via upload

---
+++ vvp7.3/vvp7.3/vvp7.3.cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+#include <locale.h>
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	float x, y, a,c,z;
+	printf("Введите x");
+	scanf("%f", &x);
+	printf("Введите A");
+	scanf("%f", &a);
+	printf("Введите Y");
+	scanf("%f", &y);
+	c = a / x;
+	z = c * y;
+	printf("1кг стоит: %f\n", c);
+	printf("%.3f кг стоит:%f\n",y, z);
+}
