8900cb6a4262ca25bfc961933fb4885dd3d2f29a
starodubova <90762340+alenka01@users.noreply.github.com>
2021-10-26 23:46:34 +0300

    Add files via upload

---
+++ vvp7.2/vvp7.2/vvp7.2.cpp
@@ -0,0 +1,14 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+#include <locale.h>
+#include < math.h >
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	float a, m;
+	printf("Введите угол в радианах");
+	scanf("%f", &a);
+	m = (float)a * 180 /3.14;
+	printf("Значение в радианах: %f\.1n", round(m));
+
+}
