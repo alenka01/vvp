d3246a72a21376dd6b154a554151fd8aac7dbed2
starodubova <90762340+alenka01@users.noreply.github.com>
2021-10-27 09:51:04 +0300

    Add files via upload

---
+++ vvp7.4/vvp7.4/vvp7.4.cpp
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+#include <locale.h>
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	float v1, v2, s, v, t,s1;
+	printf("Введите скорости первого и второго автомобиля соотвественно:");
+	scanf("%f%f", &v1, &v2);
+	printf("Введите расстояние и время соотвественно:");
+	scanf("%f%f", &s, &t);
+	v = v1 + v2;
+	s1 = v / t+s;
+	printf("Расстояние между автомобилями через %f часа равно: %f", t, s1);
+	
+}
