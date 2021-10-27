70b61c5d52ac0e7310c73bff32be9b997c545871
starodubova <90762340+alenka01@users.noreply.github.com>
2021-10-27 10:11:19 +0300

    Add files via upload

---
+++ vvp7.5/vvp7.5/vvp7.5.cpp
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+#include <locale.h>
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	float a, b, x;
+	printf("Введите коэффициенты A и B соответственно:");
+	scanf("%f%f", &a, &b);
+	if (a != 0)
+		x = -b / a;
+	else
+		printf("Error");
+	printf("Ответ:%f\n", x);
+}
